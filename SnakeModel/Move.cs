7dfea399fa9f02d30f7ce5491eb0be037334450d
devnoel <paisbnoel@gmail.com>
2017-11-16 23:05:49 +0100

    Implement and properly test Move

---
+++ SnakeModel/Move.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPSnake.Model
+{
+    class Move
+    {
+        private ITile step;
+
+        public static Move Up
+        {
+            get { return new Move(new Tile(0, -1)); }
+        }
+
+        public static Move Down
+        {
+            get { return new Move(new Tile(0, 1)); }
+        }
+
+        public static Move Left
+        {
+            get { return new Move(new Tile(-1, 0)); }
+        }
+
+        public static Move Right
+        {
+            get { return new Move(new Tile(1, 0)); }
+        }
+
+        public Move(ITile step)
+        {
+            this.step = step;
+        }
+
+        public Move(Move move)
+        {
+            step = move.step;
+        }
+
+        public Move Rotate(Direction dir)
+        {
+            Int32 newX = step.XIndex;
+            Int32 newY = step.YIndex;
+            switch (dir)
+            {
+                case Direction.Left:
+                    newX = step.YIndex;
+                    newY = -1 * step.XIndex;
+                    break;
+                case Direction.Right:
+                    newX = -1 * step.YIndex;
+                    newY = step.XIndex;
+                    break;
+            }
+            var newStep = new Tile(newX, newY);
+            return new Move(newStep);
+        }
+
+        public ITile From(ITile from)
+        {
+            return new Tile(from.XIndex + step.XIndex, from.YIndex + step.YIndex);
+        }
+
+        public override string ToString()
+        {
+            return step.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var move = obj as Move;
+            if (move == null)
+                return false;
+            else
+                return step.Equals(move.step);
+        }
+    }
+}
---
+++ SnakeModelTest/MoveTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NPSnake.Model
+{
+    /// <summary>
+    /// Summary description for MoveTest
+    /// </summary>
+    [TestClass]
+    public class MoveTest
+    {
+        public MoveTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestDefaultMoves()
+        {
+            Assert.AreEqual("[0, -1]", Move.Up.ToString());
+            Assert.AreEqual("[0, 1]", Move.Down.ToString());
+            Assert.AreEqual("[-1, 0]", Move.Left.ToString());
+            Assert.AreEqual("[1, 0]", Move.Right.ToString());
+        }
+
+        [TestMethod]
+        public void TestRotate()
+        {
+            Assert.AreEqual(Move.Left, Move.Up.Rotate(Direction.Left));
+            Assert.AreEqual(Move.Right, Move.Up.Rotate(Direction.Right));
+            Assert.AreEqual(Move.Up, Move.Up.Rotate(Direction.Forward));
+            Assert.AreEqual(Move.Up, Move.Left.Rotate(Direction.Right));
+            Assert.AreEqual(Move.Up, Move.Right.Rotate(Direction.Left));
+            Assert.AreEqual(Move.Right, Move.Down.Rotate(Direction.Left));
+        }
+
+        [TestMethod]
+        public void TestFrom()
+        {
+            Assert.AreEqual("[1, -1]", Move.Up.From(new Tile(1, 0)).ToString());
+            Assert.AreEqual("[10, 0]", Move.Right.From(new Tile(9, 0)).ToString());
+            Assert.AreEqual("[0, 0]", Move.Down.From(new Tile(0, -1)).ToString());
+        }
+    }
+}
